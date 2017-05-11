721c09a2a3e9bb68df478109b55312834fe6fc5d
vishalvyas117 <vishalvyas117@gmail.com>
2017-05-11 23:58:47 +0530

    Commit for first project to repository master

---
+++ OHB/src/main/java/com/ohb/OhbApplication.java
@@ -0,0 +1,12 @@
+package com.ohb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhbApplication.class, args);
+	}
+}
